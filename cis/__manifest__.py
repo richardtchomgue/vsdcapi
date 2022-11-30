27954ed769e225df709c338cc41d0d0678e12d47
Danish <danish@silverdaletech.com>
2022-11-30 14:20:22 +0500

    CIS

---
+++ cis/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'CIS',
+    'version': '15.0.1.0.0',
+    'summary': 'Certified Invoicing System',
+    'author': 'TMR Computing',
+    'website': 'https://www.tmrcomputing.com',
+    'license': 'OPL-1',
+    'data': ['data/cis_data.xml', 'security/ir.model.access.csv'],
+    'depends': ['stock'],
+    'installable': True,
+    'auto_install': False,
+}
